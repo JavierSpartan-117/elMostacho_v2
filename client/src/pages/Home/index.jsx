aa9032e440851e0b1ab3e7300ca4bf4c3d7d4e23
Nestor <ncalderont2@miumg.edu.gt>
2024-06-05 00:58:11 -0600

    Se instalo Tailwind y Material Tailwind

---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@material-tailwind/react';
+
+const Home = () => {
+    return (
+        <>
+            <h1>hola</h1>
+            <Button>boton</Button>
+        </>
+    );
+};
+
+export default Home;
